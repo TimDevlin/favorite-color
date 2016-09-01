c3c2ad5aa4ca14b2dc33ed1fbcf6eddc20711ed9
Tim Devlin <timdevlin@gmail.com>
2016-08-31 22:45:31 -0700

    Add favorite color project using Jquery to change backrground color

---
+++ Desktop/favorite-color/js/scripts.js
@@ -0,0 +1,20 @@
+$(function() {
+  $("button#green").click(function() {
+    $("body").removeClass()
+    $("body").addClass("background-purple");
+   });
+});
+
+$(function() {
+  $("button#yellow").click(function() {
+    $("body").removeClass()
+    $("body").addClass("background-red");
+   });
+});
+
+$(function() {
+  $("button#red").click(function() {
+    $("body").removeClass()
+    $("body").addClass("background-pink");
+   });
+});
